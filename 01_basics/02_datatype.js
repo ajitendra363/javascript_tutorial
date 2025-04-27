867cc3a63ff66ba12eb7875b95489b25f853bb86
ajitendra363 <ajitendra363@gmail.com>
2025-04-27 22:11:58 +0530

    add 02_datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,35 @@
+let name="jitendra"
+let age=19
+let isLoggedIn=true
+let state=null // null is a special value that represents the absence of any value or object
+let city=undefined // undefined is a special value that represents the absence of any value or object
+// number
+/*
+* BigInt
+* integer
+* float
+* double
+*/
+let accountId=1234567890n // BigInt is a built-in object that provides a way to represent whole numbers larger than 2^53 - 1
+let accountBalance=123456.78 // float  // float is a built-in object that provides a way to represent decimal numbers
+let accountPassword=1234567890 // integer
+let accountCity="Delhi" // string
+let accountEmail="ajitendra363" // string
+let accountIsLoggedIn=true // boolean
+let accountState=null // null is a special value that represents the absence of any value or object
+let accountCityUndefined=undefined // undefined is a special value that represents the absence of any value or object
+let accountCitySymbol=Symbol("Delhi") // Symbol is a built-in object that provides a way to create unique identifiers for objects
+let accountCityBigInt=BigInt(1234567890) // BigInt is a built-in object that provides a way to represent whole numbers larger than 2^53 - 1
+let accountCityFloat=123456.78 // float  // float is a built-in object that provides a way to represent decimal numbers
+let accountCityDouble=1234567890.1234567890 // double  // double is a built-in object that provides a way to represent decimal numbers
+let accountCityInteger=1234567890 // integer  // integer is a built-in object that provides a way to represent whole numbers
+
+// typeof operator
+console.log(typeof name) // string
+console.log(typeof age) // number
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof state) // object
+console.log(typeof city) // undefined
+console.log(typeof accountId) // bigint
+console.log(typeof accountBalance) // number
+
