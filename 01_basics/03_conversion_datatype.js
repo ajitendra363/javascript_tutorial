1a958cf5e15e91f3eda4ea1b1dc4c64fba5788bc
ajitendra363 <ajitendra363@gmail.com>
2025-04-28 16:54:14 +0530

    add 03_ datatype conversion

---
+++ 01_basics/03_conversion_datatype.js
@@ -0,0 +1,33 @@
+let score = "123" // string
+console.log(typeof score) // 123123
+
+let score1 = Number(score)// number
+console.log(typeof score1) // 123123
+
+let score = "123FDDF" // string
+console.log(typeof score) // 123123
+
+//  let score1 = Number(score)// NaN
+console.log(typeof score1) // 123123
+
+// typecasting
+// 1. String to Number
+// 2. Number to String
+// 3. String to Boolean
+// 4. Boolean to String
+// 5. Number to Boolean
+// 6. Boolean to Number
+// 7. Object to String
+// 8. String to Object
+// 9. Object to Number
+// 10. Number to Object
+// 11. Object to Boolean
+// 12. Boolean to Object
+// 13. String to Array
+// 14. Array to String
+// 15. Array to Number
+// 16. Number to Array
+// 17. Array to Boolean
+// 18. Boolean to Array
+// 19. Array to Object
+// 20. Object to Array
