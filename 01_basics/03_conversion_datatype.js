0cdd45bca3769a1ed641772f505ef79739e30b88
ajitendra363 <ajitendra363@gmail.com>
2025-04-28 20:15:07 +0530

    add 03_ datatype conversion

--- 01_basics/03_conversion_datatype.js
+++ 01_basics/03_conversion_datatype.js
@@ -31,3 +31,13 @@ console.log(typeof score1) // 123123
 // 18. Boolean to Array
 // 19. Array to Object
 // 20. Object to Array
+
+/*must remember that when we are converting a string to a number, if the string is not a number, it will return NaN.
+
+* must remember this .
+*/
+let str1=1+'1'+"6"
+comsole.log(str1) // 116
+let str2=1+1+"6"
+comsole.log(str2) // 26
+
